960fe9c204d8dac7caf0421f130488e846e461d6
Callier <45624004+YanCallier@users.noreply.github.com>
2019-03-29 17:07:42 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,29 +1,13 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 
 app.use("/drumKit", express.static(__dirname + '/drumKit'));
 app.use("/lib", express.static(__dirname + '/lib'));
-app.use(bodyParser.urlencoded({
-    extended: false
-  }));
 
 ////////////////////////////////////////////////////////////
 
 app.get('/', function (req,res){
     res.sendFile('index.html', {root: 'drumKit'});
-});
-
-app.get('/test', function (req,res){
-    res.sendFile('index_test.html', {root: 'drumKit'});
-});
-
-app.get('/phone', function (req,res){
-    res.sendFile('index.html', {root: 'phonetest'});
-});
-
-app.post('/', function(req, res){
-	console.log(req.body);
 });
 
 ////////////////////////////////////////////////////////////
